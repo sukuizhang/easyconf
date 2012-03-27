27ec09b8f6bf02f22785921297b58ff37c5a9486
Martin <zxm@basecity.com>
2012-03-27 15:45:21 +0800

    try to simplify the easyconf implement

--- src/easyconf/confs.clj
+++ src/easyconf/confs.clj
@@ -1,105 +1,82 @@
 (ns easyconf.confs
-  (:require [resource-monitor.core :as monitor]
-            [clojure.set :as set]
-            [clojure.tools.logging :as logging])
-  (:import [java.io File FileInputStream InputStreamReader BufferedReader]))
+  (:require [clojure.set :as set]
+            [clojure.tools.logging :as logging]))
 
-(comment "all config vars, normally it add in when compile source file")
-(def ^:private conf-vars (atom nil))
-(comment
-  "all config values, it load from config path when first use, auto reload when config file has changed.
-   when config value first load or changed, it will inject to according config var.")
-(def ^:private conf-vals (atom nil))
-(comment
-  "when config value of :no-useless-config has set, no useless config item check option is open
-   when config value of :all-var-must-config has set, all var must be config check option is open")
-(defonce ^:private check-options [:no-useless-config :all-var-must-config])
+
+;;every conf-name correspends to a unique clojure var
+;;when the value of a speify conf-name changes, it will automaticly change
+;;the value of the var
+
+;;{conf-name1 conf-value1 conf-name2 conf-value2}
+(def ^:private conf-vals (atom {}))
+;;{conf-name1 conf-var1 conf-name2 conf-var2}
+(def ^:private conf-vars (atom {}))
+
+(defn reset-conf-vals
+  "clear all config values"
+  []
+  (reset! conf-vals (atom {})))
+
+(defn get-conf-name
+  [var]
+  (keyword (or (:conf-name (meta var)) (.sym var))))
 
 (defn validate-err
   "throw a Exception when validate fail."
-  [var conf msg]
-  (if msg
-    (let [{:keys [file-name ns conf-name value]} conf]    
-      (throw (Exception.
-              (str "error load conf [file name:" file-name
-                   " conf name:" conf-name
-                   " value:" value "]"    
-                   "\nvalidate fail for " var
-                   "\nmsg: " msg))))))
+  [var f value msg]
+  (when-not (f value) 
+    (throw (RuntimeException.
+            (str "error load config , conf-name " (get-conf-name var)
+                 " value:" value     
+                 "to " var
+                 "\nmsg: " msg)))))
 
-(defn set-var
-  "change root binding of the var, a validate will be take if it announce on the var use :validator meta, and you can assign validate fail message.
- when your config value is string and var type is not, it load this config string to clojure data and use it as new value. var type from :tag meta or the initial var value, if not, it guess it as string"
-  [var conf]
-  (logging/info (str "inject config value:" conf " to var:" var))
+(defn set-var-value
+  "change root binding of the var, a validate will be take if it announce on the var use :validator meta, and you can assign validate fail message."
+  [var value]
+  (logging/info (str "inject config value:" value " to var:" var))
   (let [m-var (meta var)
-        require-type (or (m-var :tag (type (var-get var)))
-                         String)
-        value (:value conf)
-        value (if (and (not= String require-type)
-                       (= String (type value)))
-                (load-string value)
-                value)
         validator (:validator m-var)
-        validate-msg (and validator
-                          (let [result (validator value)]
-                            (or (and (string? result) result)
-                                (and (not (nil? result)) (not result) (m-var :validate-msg "validate fail !")))))]
-    (validate-err var conf validate-msg)
+        validate-msg (:validator-msg m-var)]
+    (validate-err var validator value validate-msg)
     (.bindRoot var value)
     var))
 
-(defn add-var!
-  "add a var to be config"
-  [var]
-  (let [key (keyword (or (:conf-name (meta var)) (.sym var)))
-        conf (key @conf-vals)]
-    (if conf
-      (logging/info (str "add config var:" var " inject value from:" conf))
-      (logging/info (str "add config var:" var " no config value found !")))
-    (swap! conf-vars assoc key var)
-    (when (not= clojure.lang.Var$Unbound (type (var-get var)))
-      (if conf
-        (logging/info (str "default config value:" (var-get var) " be overrided with config item" conf))
-        (do
-          (logging/info (str "no config value found, use default config value:" (var-get var)))
-          (swap! conf-vals assoc key (var-get var)))))
-    (if conf (set-var var conf))))
+(defn config-value
+  "change the var value"
+  [conf-name value]
+  (swap! conf-vals assoc conf-name value)
+  (when (find @conf-vars conf-name)
+    (set-var-value (conf-name @conf-vars) value)))
 
-(defn add-conf-value
-  "add a config value"
-  [file-name ns conf-name value]
-  (let [key (keyword conf-name)
-        var (key @conf-vars)
-        conf {:file-name file-name :ns ns :conf-name conf-name :value (var-get value)}]
-    (logging/info (str "add config item:" conf " inject to var:" var))
-    (swap! conf-vals assoc key conf)
-    (if var (set-var var conf))))
+(defn load-ns
+  "Require all the namespaces prefixed by the namespace symbol given so that the dict and conf are loaded."
+  [& ns-syms]
+  (doseq [ns-sym ns-syms
+          n (namespace/find-namespaces-on-classpath)
+          :let [pattern (re-pattern (name ns-sym))]
+          :when (re-seq pattern (name n))]
+    (require n)))
+
+(defn add-var!
+  "add a var to be configured"
+  [var]
+  (let [conf-name (get-conf-name var)
+        unbound (= clojure.lang.Var$Unbound (type (var-get var)))]
+    (swap! conf-vars assoc conf-name var)
+    (if unbound
+      (if (not (find @conf-vals conf-name))
+        (throw (RuntimeException. (str "var: " var " can not find config value")))
+        (logging/info "var " var
+                      " cannot find config value , using default: "
+                      (var-get var)))
+      (set-var-value var (conf-name @conf-vals)))))
 
 (defn check
-  "check if config items is according to define config vars"
+  "check if all config values are used"
   []
-  (let [errors {:useless (and ((first check-options) @conf-vals)
-                              (set/difference
-                               (set (keys @conf-vals))
-                               (set (keys @conf-vars))))
-                :config-not-found (and ((first check-options) @conf-vals)
-                                       (set/difference
-                                        (set (keys @conf-vars))
-                                        (set (keys @conf-vals))))}
-        not-option (fn [key] (not-any? #(= key %) check-options))
-        error-msgs (-> errors
-                       (assoc-in [:useless] (->> (:useless errors)
-                                              (filter not-option)
-                                              (map (fn [key] (str "useless config item:" (key @conf-vals))))))
-                       (assoc-in [:config-not-found] (->> (:config-not-found errors)
-                                                     (filter not-option)
-                                                     (map (fn [key] (str "not config var:" (key @conf-vars)))))))
-        msg (->> (or (and (< 0 (count (:config-not-found error-msgs)))
-                  (apply conj (:useless error-msgs) (:config-not-found error-msgs)))
-             (:useless error-msgs))
-         (interpose "\n")
-         (apply str)
-         (#(if (< 0 (count %)) (str "check error msgs:\n" %))))]
-    (if msg
-      (throw (Exception. msg)))))
+  (let [vals-keys (set (keys @conf-vals))
+        vals-vars (set (keys @conf-vars))
+        diff (set/difference vals-keys vars-keys)]
+    (when-not (seq diff)
+      (throw (RuntimeException. (str "config " diff " haven't be used" ))))))
--- src/easyconf/core.clj
+++ src/easyconf/core.clj
@@ -1,12 +1,28 @@
 (ns easyconf.core
   (:require [easyconf.confs :as confs]
-            [easyconf.load :as load]))
+   [clojure.tools.namespace :as namespace]))
+
+(defn config
+  "set conf-name to value conf-value, if check-duplicate is ture
+   then every conf-name can only be set only once"
+  ([conf-name conf-value]
+     (config config-name conf-value false))
+  ([conf-name value check-duplicate]
+     (if (and check-duplicate (find @conf-vals conf-name))
+       (throw (RuntimeException.
+               (str "conf-name : " conf-name " duplicated")))
+       (confs/change-value conf-name value))))
+
+(defn config-once
+  "convenient function to force every conf-name can only be configured once,
+   otherwiese will cause a exception"
+  [conf-name conf-value]
+  (config conf-name conf-value true))
 
 (defmacro defconf
   "use it just like (def ...), and it will inject config value to def var."
   [& form]
   `(do
-     (easyconf.load/load-conf-path)
      (let [var# (def ~@form)]
          (easyconf.confs/add-var! var#) var#)))
 
@@ -14,6 +30,9 @@
   "use it just like (defn ...), and it will inject config value to def var."
   [& form]
   `(do
-     (easyconf.load/load-conf-path)
      (let [var# (defn ~@form)]
          (easyconf.confs/add-var! var#) var#)))
+
+;;all configure file must be put under easyconf.conf namespace
+;;preload to guarantee the config value be loaded first.
+(confs/load-ns 'easyconf.conf)
