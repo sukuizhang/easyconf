370f7f9fe282516c03b55927b1a7a2c6a20b1c3e
Martin <zxm@basecity.com>
2012-03-28 16:08:10 +0800

    revert to simplify

--- example/etc/b.clj
+++
@@ -1,5 +0,0 @@
-(def x 12)
-(def y "xyz")
-(def z [1 2 3])
-(defn f [x] (* x x))
-(def g "eeesa")
--- example/project.clj
+++
@@ -1,5 +0,0 @@
-(defproject example "1.0.0"
-  :description "example"
-  :extra-classpath-dirs ["../src"]
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [resource-monitor "0.0.1"]])
